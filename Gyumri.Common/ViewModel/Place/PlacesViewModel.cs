ebbfb997fc90dd2b7ffd5b6c620b649271a04b08
Inesa Mkrtchyan <inesamkrtchyan9999@gmail.com>
2025-05-04 05:56:35 +0400

    plan a trip

--- Gyumri.Common/ViewModel/Place/PlacesViewModel.cs
+++ Gyumri.Common/ViewModel/Place/PlacesViewModel.cs
@@ -16,6 +16,8 @@ namespace Gyumri.Common.ViewModel.Place
         public string DescriptionArm { get; set; }
         public string DescriptionRu { get; set; }
         public string Photo { get; set; }
+        public int MinPrice { get; set; }
+        public int MaxPrice { get; set; }
         public int SubcategoryId { get; set; }
     }
 }
--- GyumriFinalVersion/Controllers/TripController.cs
+++ GyumriFinalVersion/Controllers/TripController.cs
@@ -5,6 +5,8 @@ using System.Net.Mail;
 using System.Net;
 using DinkToPdf.Contracts;
 using DinkToPdf;
+using Gyumri.Application.Services;
+using System.Drawing.Printing;
 
 namespace GyumriFinalVersion.Controllers
 {
@@ -13,12 +15,17 @@ namespace GyumriFinalVersion.Controllers
         private const string CultureCookieName = "UserCulture";
         private readonly ApplicationContext _context;
         private readonly ICategory _categoryService;
+        private readonly ISubcategory _subCategoryService;
+        private readonly IPlace _placeService;
+
         private readonly IConverter _converter;
 
-        public TripController(ICategory categoryService, ApplicationContext context)
+        public TripController(ICategory categoryService, ApplicationContext context, IPlace placeServoce, ISubcategory subcategoryService)
         {
             _categoryService = categoryService;
-            _context = context;        
+            _context = context;
+            _subCategoryService = subcategoryService;
+            _placeService = placeServoce;
         }
 
         [HttpGet]
@@ -33,23 +40,78 @@ namespace GyumriFinalVersion.Controllers
             ViewBag.Categories = await _categoryService.GetAllCategories();
             return View();
         }
-        public async Task<IActionResult> FirstStep()
-        {
 
-            ViewBag.Categories = await _categoryService.GetAllCategories();
-            return View();
-        }
-        public async Task<IActionResult> SecondStep(int categoryId)
+        [HttpPost]
+        public async Task<IActionResult> Index(string dates, string adult, string children)
         {
             ViewBag.Categories = await _categoryService.GetAllCategories();
             var currentCulture = Request.Cookies["UserCulture"] ?? "en";
             var cultureInfo = new System.Globalization.CultureInfo(currentCulture);
             Thread.CurrentThread.CurrentCulture = cultureInfo;
             Thread.CurrentThread.CurrentUICulture = cultureInfo;
+            if (dates is null && adult is null && children is null)
+            {
+                return View();
+            }
+            TempData["Dates"] = dates;
+            TempData["AductCount"] = adult;
+            TempData["ChildrenCount"] = children;
+
+            ViewBag.Categories = await _categoryService.GetAllCategories();
+            return RedirectToAction("FirstStep");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> FirstStep()
+        {
 
             ViewBag.Categories = await _categoryService.GetAllCategories();
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> FirstStep(string transport)
+        {
+            ViewBag.Categories = await _categoryService.GetAllCategories();
+            TempData[transport] = transport;
+
+            return RedirectToAction("SecondStep");
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> SecondStep(int subcategoryId = -1, int currentPage = 1)
+        {
+            var categories = await _categoryService.GetAllCategories();
+            var currentCulture = Request.Cookies["UserCulture"] ?? "en";
+            var cultureInfo = new System.Globalization.CultureInfo(currentCulture);
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+            var seeAndDoCategory = categories.FirstOrDefault(c => c.Name == "Relax & Sleep");
+            var subcategories = await _subCategoryService.GetSubcategoriesByCategoryId(seeAndDoCategory.CategoryId);
+            if (subcategoryId == -1)
+            {
+                subcategoryId = subcategories.First().SubcategoryId;
+            }
+            var places = await _placeService.GetPlacesBySubCategoryId(subcategoryId);
+            if (places.Count() > 5)
+            {
+                ViewBag.PagesCount = (int)Math.Ceiling((double)places.Count() / 5);
+            }
+            else
+            {
+                ViewBag.PagesCount = 1;
+            }
+            ViewBag.AllSubcategories = subcategories;
+            ViewBag.CurrentSubcategory = subcategories.FirstOrDefault(sc => sc.SubcategoryId == subcategoryId);
+            ViewBag.CurrentPage = currentPage;
+            ViewBag.Places = places.Skip((currentPage - 1) * 5)
+                                   .Take(5)
+                                   .ToList();
+            ViewBag.Categories = categories;
+            return View();
+        }
+
         public async Task<IActionResult> ThirdStep(int categoryId)
         {
             ViewBag.Categories = await _categoryService.GetAllCategories();
