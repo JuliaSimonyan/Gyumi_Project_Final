1120588157b32352f41e2c24b63559764688cfd4
Inesa Mkrtchyan <inesa.mkrtchyan@armsoft.am>
2025-05-23 09:29:31 +0400

    fixes

---
+++ GyumriFinalVersion/Models/TripInputInfo.cs
@@ -0,0 +1,17 @@
+﻿using Gyumri.Common.ViewModel.Place;
+
+namespace GyumriFinalVersion.Models
+{
+    public class TripInputInfo
+    {
+        public string TransportName { get; set; }
+        public string TransportAmount { get; set; }
+        public string TimePeriod { get; set; }
+
+        public string Date { get; set; }
+        public string ChildrenCount { get; set; }
+        public string AdultCount { get; set; }
+        public PlacesViewModel PlaceWhereToStay { get; set; }
+        public List<PlacesViewModel> PlaceWhatToDo { get; set; }
+    }
+}
